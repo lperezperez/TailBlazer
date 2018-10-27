483f1b91334691d6d3dc57f59f115b4e568bb553
Luis Pérez <lperezperez@gmail.com>
2018-10-27 21:06:29 +0200

    Update .NET Framework version to 4.7.2

--- Source/TailBlazer.Domain/Properties/AssemblyInfo.cs
+++ Source/TailBlazer.Domain/Properties/AssemblyInfo.cs
@@ -1,35 +1,11 @@
 ﻿using System.Reflection;
 using System.Runtime.InteropServices;
-
-// General Information about an assembly is controlled through the following 
-// set of attributes. Change these attribute values to modify the information
-// associated with an assembly.
-[assembly: AssemblyTitle("Tail Blazer")]
+[assembly: AssemblyCopyright("© 2018 Roland Pheasant")]
 [assembly: AssemblyDescription("Tail blazer is a file tail utility")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyFileVersion("0.0.0.2")]
+[assembly: AssemblyInformationalVersion("0.0")]
 [assembly: AssemblyProduct("TailBlazer")]
-[assembly: AssemblyCopyright("© 2018")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
-
-// Setting ComVisible to false makes the types in this assembly not visible 
-// to COM components.  If you need to access a type in this assembly from 
-// COM, set the ComVisible attribute to true on that type.
+[assembly: AssemblyTitle("Tail Blazer")]
+[assembly: AssemblyVersion("0.0.0")]
 [assembly: ComVisible(false)]
-
-// The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("332f8beb-1743-4b41-8953-a66a57f2503d")]
-
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers 
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("0.0.0.1")]
-[assembly: AssemblyFileVersion("0.0.0.1")]
