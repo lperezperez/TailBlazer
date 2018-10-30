20655642a2a063a3569c3bf779ba8a2e31bae945
Luis Pérez <lperezperez@gmail.com>
2018-10-30 01:21:05 +0100

    Enhace font selector in settings

--- Source/TailBlazer/Views/Options/GeneralOptionsViewModel.cs
+++ Source/TailBlazer/Views/Options/GeneralOptionsViewModel.cs
@@ -1,14 +1,13 @@
 namespace TailBlazer.Views.Options
 {
-    using DynamicData.Binding;
     using System;
     using System.Diagnostics;
-    using System.Linq;
     using System.Reactive.Disposables;
     using System.Reactive.Linq;
     using System.Windows;
     using System.Windows.Input;
     using System.Windows.Media;
+    using DynamicData.Binding;
     using TailBlazer.Domain.Formatting;
     using TailBlazer.Domain.Infrastructure;
     using TailBlazer.Domain.Settings;
@@ -17,9 +16,9 @@ namespace TailBlazer.Views.Options
     {
         #region Fields
         private readonly IDisposable cleanUp;
-        private string logFont;
         private double highlightDuration;
         private bool highlightTail;
+        private string logFont;
         private bool openRecentOnStartup;
         private int rating;
         private double scale;
@@ -29,11 +28,12 @@ namespace TailBlazer.Views.Options
         #region Constructors
         public GeneralOptionsViewModel(ISetting<GeneralOptions> setting)
         {
-            var reader = setting.Value.Subscribe(
+            var reader = setting.Value.Subscribe
+                (
                  options =>
                      {
                          this.UseDarkTheme = options.Theme == Theme.Dark;
-                         this.LogFont = new FontFamily(options.LogFont);
+                         this.LogFont = options.LogFont;
                          this.HighlightTail = options.HighlightTail;
                          this.HighlightDuration = options.HighlightDuration;
                          this.Scale = options.Scale;
@@ -42,13 +42,14 @@ namespace TailBlazer.Views.Options
                          this.ShowLineNumbers = options.ShowLineNumbers;
                      });
             this.RequiresRestart = setting.Value.Select(options => options.Rating).HasChanged().ForBinding();
-            this.RestartCommand = new Command(
+            this.RestartCommand = new Command
+                (
                  () =>
                      {
                          Process.Start(Application.ResourceAssembly.Location);
                          Application.Current.Shutdown();
                      });
-            var writer = this.WhenAnyPropertyChanged().Subscribe(vm => { setting.Write(new GeneralOptions(this.UseDarkTheme ? Theme.Dark : Theme.Light, this.LogFont.Source, this.HighlightTail, this.HighlightDuration, this.Scale, this.Rating, this.OpenRecentOnStartup, this.ShowLineNumbers)); });
+            var writer = this.WhenAnyPropertyChanged().Subscribe(vm => { setting.Write(new GeneralOptions(this.UseDarkTheme ? Theme.Dark : Theme.Light, this.LogFont, this.HighlightTail, this.HighlightDuration, this.Scale, this.Rating, this.OpenRecentOnStartup, this.ShowLineNumbers)); });
             this.HighlightDurationText = this.WhenValueChanged(vm => vm.HighlightDuration).DistinctUntilChanged().Select(value => value.ToString("0.00 Seconds")).ForBinding();
             this.ScaleText = this.WhenValueChanged(vm => vm.Scale).DistinctUntilChanged().Select(value => $"{value} %").ForBinding();
             this.ScaleRatio = this.WhenValueChanged(vm => vm.Scale).DistinctUntilChanged().Select(value => (decimal)value / 100).ForBinding();
@@ -59,6 +60,15 @@ namespace TailBlazer.Views.Options
         public double HighlightDuration { get => this.highlightDuration; set => this.SetAndRaise(ref this.highlightDuration, value); }
         public IProperty<string> HighlightDurationText { get; }
         public bool HighlightTail { get => this.highlightTail; set => this.SetAndRaise(ref this.highlightTail, value); }
+        public string LogFont
+        {
+            get => this.logFont;
+            set
+            {
+                if (value != null)
+                    this.SetAndRaise(ref this.logFont, value);
+            }
+        }
         public bool OpenRecentOnStartup { get => this.openRecentOnStartup; set => this.SetAndRaise(ref this.openRecentOnStartup, value); }
         public int Rating { get => this.rating; set => this.SetAndRaise(ref this.rating, value); }
         public IProperty<bool> RequiresRestart { get; }
@@ -68,7 +78,6 @@ namespace TailBlazer.Views.Options
         public IProperty<string> ScaleText { get; }
         public bool ShowLineNumbers { get => this.showLineNumbers; set => this.SetAndRaise(ref this.showLineNumbers, value); }
         public bool UseDarkTheme { get => this.useDarkTheme; set => this.SetAndRaise(ref this.useDarkTheme, value); }
-        public FontFamily LogFont { get => new FontFamily(this.logFont); set => this.SetAndRaise(ref this.logFont, value.Source); }
         #endregion
         #region Methods
         void IDisposable.Dispose() => this.cleanUp.Dispose();
